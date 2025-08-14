37131a8ea950645bb22418d266740ae3f7ca6f72
DavutOvez <dawut2010owezow@gmail.com>
2025-08-14 23:16:06 +0500

    create views

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from .forms import*
+
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request,'blog/home.html',{'posts':posts})
+
+
+def post_detail(request,id):
+    post = get_object_or_404(Post,id=id)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            return redirect('post_detail',id=post.id)
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post_detail.html',{'post':post,'comments':comments,'comment_form':comment_form})
